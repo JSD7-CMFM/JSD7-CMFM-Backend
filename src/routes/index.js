0044f29b38b7ada524ea31a1e5178d1cc9f1b3f6
Modey Codey <phawngam@hotmail.com>
2024-07-04 15:52:24 +0700

    orderapi

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,9 @@
 import express from "express";
 import { getUsers } from "../controllers/usersController.js";
-import {
-  getOrders,
-  getOrder,
-  updateOrder,
-} from "../controllers/ordersController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
-router.get("/orders", getOrders);
-router.get("/orders/:id", getOrder);
-router.patch("/orders/:id", updateOrder);
 
 router.get("/products", (req, res) => {
   res.send("Hello from Products!");
