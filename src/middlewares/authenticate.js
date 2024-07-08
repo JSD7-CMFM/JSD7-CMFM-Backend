d1455b773ef3f58fab634589bc7e393a500b75df
Bongkan <bongkan.jj@gmail.com>
2024-07-08 10:41:31 +0700

    edit user controller and authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -15,13 +15,13 @@ const authenticate = async (req, res, next) => {
     const authorization = req.headers?.authorization.startsWith("Bearer")
       ? req.headers.authorization
       : next(new Error("Not found Bearer token", "InvalidToken", 400));
-
+    console.log("authorization", authorization);
     const token = authorization.split(" ")[1]
       ? authorization.split(" ")[1]
       : next(new Error("Not found Bearer token", "InvalidToken", 400));
-
+    console.log("token", token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || "key");
-
+    console.log("decoded", decoded);
     if (!mongoose.Types.ObjectId.isValid(decoded.id)) {
       throw new Error("Invalid ID format", "InvalidID", 400);
     }
@@ -30,12 +30,12 @@ const authenticate = async (req, res, next) => {
     if (!user) {
       next(new Error("Your account has been deleted", "NotFoundData", 500));
     }
-
     // if (!user.isAdmin) {
     //   next(new Error("No permission, you are not an admin", "NotAdmin", 403));
     // }
 
     req.user = user;
+    console.log("req.user", req.user);
     next();
   } catch (err) {
     next(err);
