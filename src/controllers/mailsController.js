a70a35bba81393662aff66af44a64f7941b03630
redrumkumg <n.ronnapat@github.com>
2024-07-13 10:15:39 +0700

    edit email form

--- src/controllers/mailsController.js
+++ src/controllers/mailsController.js
@@ -20,36 +20,41 @@ const mailController = {
       // หา email ของผู้ใช้จาก orderId
       const email = await mailService.getEmailUser(orderId);
       const { cart_products } = await orderService.getOrderById(orderId);
-      console.log(cart_products);
+      const order = await orderService.getOrderById(orderId);
       if (!email) {
         return res.status(404).json({ message: "User not found" });
       }
-
+    
       // สร้างข้อมูลอีเมล
       const mailOptions = {
         from: process.env.MAIL_SENDER_ADMIN, // ผู้ส่งอีเมล
         to: email, // ผู้รับอีเมล
         subject: "Order Confirmation", // หัวข้ออีเมล
         html: `
+          <h1>PONY MART : JSD 7</h1>
           <h1>Order Confirmation</h1>
-          <p>Your order with ID ${orderId} has been confirmed.</p>
+          <div style='padding-bottom: 10px'>Your order with ID ${orderId} has been confirmed.</div>
+          <div style='padding-bottom: 10px'>Total price :  ${order.total_price} THB</div>
           <ul>
-            ${cart_products.map(
-              (product) => `
+            ${cart_products
+              .map(
+                (product) => `
               <li>
-                <img src="${product.product_img}" alt="${product.name}">
-                <h3>${product.name}</h3>
-                <p>Quantity: ${product.amount}</p>
-                <p>Price: ${product.price}</p>
+                <img src="${product.product_img}" alt="${product.name}" width="200">
+                <div style='padding-bottom: 10px'>${product.name}</div>
+                <div style='padding-bottom: 10px'>Quantity: ${product.amount}</div>
+                <div style='padding-bottom: 10px'>Price per unit: ${product.price}</div>
               </li>
-            `
-            )}
+              `
+              )
+              .join("")}
+          </ul>
         `,
         // text: `Your order with ID ${orderId} has been confirmed.`, // เนื้อหาอีเมล
       };
 
       // ส่งอีเมล
-        await transporter.sendMail(mailOptions);     
+      await transporter.sendMail(mailOptions);
       console.log("Email sent successfully");
       res.status(200).json({ message: "Email sent successfully" });
     } catch (error) {
